3e6cc7ad580b552470d6d748e58892b68f580ce2
kiruthigarajendran1903 <kiruthiga2807@gmail.com>
2023-07-19 16:01:26 +0530

    Filehandling commit

---
+++ FilleHandling.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+
+namespace ConAppAssignment10
+{
+    public class FilleHandling
+    {
+        public static void CreateFile(string fileName, string content)
+        {
+            string filePath = fileName + ".txt";
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("File already Exists");
+            }
+            else
+            {
+                File.WriteAllText(filePath, content);
+                Console.WriteLine("File Created Succesfully");
+            }
+        }
+        public static string ReadFile(string FName)
+        {
+
+            string filePath = FName + ".txt";
+                return File.ReadAllText(filePath);
+            }
+           
+        
+        public static void AppendToFIle(string fileName, string ConTent)
+        {
+            string filePath = fileName + ".txt";
+            if (File.Exists(filePath))
+            {
+                File.AppendAllText(filePath, ConTent);
+                Console.WriteLine("Content appended successfully");
+            }
+            else
+            {
+                Console.WriteLine("File Not Avilable");
+            }
+        }
+        public static void DeleteFIle(string fileName)
+        {
+            string filePath = fileName + ".txt";
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+                Console.WriteLine("File Deleted Successfully");
+            }
+            else
+            {
+                Console.WriteLine("File not Avilable");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+
+namespace ConAppAssignment10
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Select the Operation");
+                Console.WriteLine("1.Create a File\n2.Read a File \n3.Append to a File\n4.Delete a File");
+                
+                string input;
+                input = Console.ReadLine();
+                
+                    switch (input)
+                    {
+
+                        case "1":
+
+                            string fPAth;
+                            Console.WriteLine("Enter File Path");
+                            fPAth = Console.ReadLine();
+                            String Content;
+                            Console.WriteLine("Enter the Content");
+                            Content = Console.ReadLine();
+                          
+                            FilleHandling.CreateFile(fPAth, Content);
+                            break;
+
+                        case "2":
+                            Console.WriteLine("Enter the file name:");
+                            string fName = Console.ReadLine();
+
+                            string fileContent = FilleHandling.ReadFile(fName);
+                            Console.WriteLine(fileContent);
+                        Console.WriteLine("File Readed Successfully");
+                        break;
+
+                        case "3":
+                            Console.WriteLine("Enter File Path");
+                            string FPath = Console.ReadLine();
+                            Console.WriteLine("ENter File Content");
+                            string ConTent = Console.ReadLine();
+                            FilleHandling.AppendToFIle(FPath, ConTent);
+                            break;
+
+                        case "4":
+                            Console.WriteLine("Enter File NAme");
+                            String DeleFile = Console.ReadLine();
+                            FilleHandling.DeleteFIle(DeleFile);
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                    }
+                
+
+                    Console.ReadKey();
+                }
+        }
+        }
+    }
+
